d52dcf189260bb18f9e2fd248ac356433c9d8b82
Makito Mori <mkt-engr@gmail.com>
2022-12-19 23:36:01 +0900

    MUI使ってフォーム書いてみる

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
+import WithMUI01 from "./WithMUI01";
+import "../style/styles.css";
 
 const router = createBrowserRouter([
   { path: "/", element: <App /> },
   {
     path: "/test",
     element: <div>Hello world!</div>,
+  },
+  {
+    path: "/mui01",
+    element: <WithMUI01 />,
   },
 ]);
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
